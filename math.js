0ef942e53af3a40700a1298bd5eb6939fb3b5fa4
Mangotrident <88416293+Mangotrident@users.noreply.github.com>
2025-07-06 20:15:06 -0700

    math.js

---
+++ math.js
@@ -0,0 +1,30 @@
+function double(n) {
+    return n * 2;
+}
+console.log(double(4));
+
+
+function square(a) {
+    return a**2;
+}
+console.log(square(9));
+
+function isEven(e) {
+    return e % 2 == 0;
+}
+console.log(isEven(4));
+
+function isOdd(o) {
+    return o % 2 != 0;
+}
+console.log(isOdd(9));
+
+function multiply(a,b) {
+    return a*b;
+}
+console.log(multiply(5,6));
+
+function cube(c) {
+    return c**3;
+}
+console.log(cube(5));
